9a731f77b310c13d6262bb4007127c93e929dfb1
Nishant <nbisen88@gmail.com>
2014-05-28 20:27:07 -0700

    OOAD- views
    Contains all the view or UI made till now

---
+++ COEN275Project/src/Constants.java
@@ -0,0 +1,9 @@
+
+public class Constants {
+
+	public static final String MYSQL_USERNAME = "root";
+	public static final String MYSQL_PASSWORD = "";
+	public static final String DATABASE = "coen275";
+	public static final String LOGIN_TABLE = "login";
+	public static final String PROJECT_DETAILS_TABLE = "project_details";
+}
