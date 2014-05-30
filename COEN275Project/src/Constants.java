f0ab784cc6dd68e0744a0f89bdf3a7c0dfe81133
ksunika <ksenia.serbina@gmail.com>
2014-05-30 16:08:06 -0700

    a

--- COEN275Project/src/Constants.java
+++
@@ -1,9 +0,0 @@
-
-public class Constants {
-
-	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "";
-	public static final String DATABASE = "coen275";
-	public static final String LOGIN_TABLE = "login";
-	public static final String PROJECT_DETAILS_TABLE = "project_details";
-}
